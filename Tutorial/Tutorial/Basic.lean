d4c5d5e4a76120545a72c1a949e8f5600469370f
c-ato <145376245+c-ato@users.noreply.github.com>
2025-12-12 12:51:22 +0000

    the first

--- Tutorial/Tutorial/Basic.lean
+++ Tutorial/Tutorial/Basic.lean
@@ -1,1 +1,17 @@
 def hello := "world"
+
+#eval hello
+
+#eval "Hello, " ++ hello ++ "!"
+#eval String.length hello
+
+def even (n : Int) : Bool :=
+  n % 2 == 0
+
+#eval 42.6 + 0.4
+
+#eval String.append "Hello, " "Lean!"
+
+#eval String.append "great " (String.append "oak " "tree")
+
+#eval String.append (String.append "A" "B") "C"
