99dc1d8f93d60776d33f05a3f5b5df9e24fcc647
c-ato <145376245+c-ato@users.noreply.github.com>
2026-01-04 22:30:10 +0000

    1.6.5 3/5 questions

---
+++ Tutorial/Excercises/1.6.5 excercises.lean
@@ -0,0 +1,52 @@
+-- Write a function to find the last entry in a list. It should return an Option.
+
+def lastentry {a : Type} (list : List a) : Option a :=
+  match list with
+  | [] => none
+  | List.cons y ys  =>
+    match lastentry ys with
+    | none => y
+    | some z => some z
+
+
+#eval lastentry [1,2,3,8,4]
+
+-- Write a function that finds the first entry in a list that satisfies a given predicate. Start the definition with def List.findFirst? {α : Type} (xs : List α) (predicate : α → Bool) : Option α := ….
+
+def List.findFirst? {α : Type} (xs : List α) (predicate : α → Bool) : Option α :=
+  match xs with
+  | [] => none
+  | List.cons y ys =>
+    match predicate y with
+    | false => findFirst? ys predicate
+    | true => some y
+
+-- Testing findfirst? with isEven and isVowel predicates
+
+def isEven (n : Int) : Bool :=
+  match n/2 with
+  | 0 => false
+  | (m : Int) => true
+
+
+def isVowel (s : String) : Bool :=
+  match s with
+  | "a" => true
+  | "e" => true
+  | "i" => true
+  | "o" => true
+  | "u" => true
+  | "y" => true
+  | _ => false
+
+#eval List.findFirst? [1,2,3,8,4] isEven
+
+#eval List.findFirst? ["b","c","d","e"] isVowel
+
+-- Write a function Prod.switch that switches the two fields in a pair for each other. Start the definition with def Prod.switch {α β : Type} (pair : α × β) : β × α := ….
+
+def Prod.switch {α β : Type} (pair : α × β) : β × α :=
+  match pair with
+  | {fst := a, snd := b} => {fst := b, snd := a}
+
+
