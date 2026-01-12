cccd8b113947d025bea66f9e4be578eb4c3b256a
c-ato <145376245+c-ato@users.noreply.github.com>
2026-01-12 11:51:23 +0000

    78

--- Tutorial/Excercises/1.6.5 excercises.lean
+++ Tutorial/Excercises/1.6.5 excercises.lean
@@ -62,3 +62,40 @@ inductive Petnames: Type where
 
 
 def animalname : List Petnames := [Petnames.dogname "Bob", Petnames.dogname "Cat", Petnames.catname "Dog"]
+
+
+-- Write a function zip that combines two lists into a list of pairs. The resulting list should be as long as the shortest input list. Start the definition with def zip {α β : Type} (xs : List α) (ys : List β) : List (α × β) := ….
+
+def zip {α β : Type} (xs : List α) (ys : List β) : List (α × β) :=
+  match xs with
+  | [] => []
+  | List.cons x' xs' =>
+    match ys with
+    | [] => []
+    | List.cons y' ys' => List.cons (x', y') (zip xs' ys')
+
+def l1 : List String := ["Spot","Cat","Rex","Bob"]
+
+def l2 : List String := ["Snow","Dog"]
+
+#eval zip l1 l2
+
+-- Write a polymorphic function take that returns the first nn entries in a list, where nn is a Nat. If the list contains fewer than nn entries, then the resulting list should be the entire input list. #eval take 3 ["bolete", "oyster"] should yield ["bolete", "oyster"], and #eval take 1 ["bolete", "oyster"] should yield ["bolete"].
+
+def First_n_Entries {a} (n : Nat) (xs : List a) : List a :=
+  match n with
+  | 0 => []
+  | Nat.succ m =>
+    match xs with
+    | [] => []
+    | List.cons x' xs' => List.cons x' (First_n_Entries m xs')
+
+#eval First_n_Entries 4 l1
+
+-- Using the analogy between types and arithmetic, write a function that distributes products over sums. In other words, it should have type α × (β ⊕ γ) → (α × β) ⊕ (α × γ).
+
+def distributivity {α β γ} (a : α × (β ⊕ γ)) : (α × β) ⊕ (α × γ) :=
+  match a with
+  | (x, p) =>
+    match p with
+    | Sum.inl, Sum.inr => Sum.inl (x, y), Sum.inr (x, z)
